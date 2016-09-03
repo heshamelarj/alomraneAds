8f11898fd41ec6dad7fd00ee4e88f21444a100a9
KonshensX <mohamedbaza16@gmail.com>
2016-09-04 00:55:09 +0100

    AOS && light box added
    shit almost done my g

--- js/main.js
+++ js/main.js
@@ -13,4 +13,95 @@ $(document).ready(function(){
                     $('.nav > li > a').css('padding', '15px 30px');
                 }
             });
-        });
+
+    var gallery = $('.gallery a').simpleLightbox();
+
+    $.simpleLightbox({
+
+// shows fullscreen overlay
+            overlay:    true,
+
+// shows loading spinner
+            spinner:    true,
+
+// shows navigation arrows
+            nav:      true,
+
+// text for navigation arrows
+            navText:    ['&larr;','&rarr;'],
+
+// shows image captions
+            captions:   true,
+            captionDelay:   0,
+            captionSelector:  'img',
+            captionType:    'attr',
+            captionPosition:  'bottom',
+
+// captions attribute (title or data-title)
+            captionsData: 'title',
+
+// shows close button
+            close:      true,
+
+// text for close button
+            closeText:    'X',
+
+// show counter
+            showCounter:  true,
+
+// file extensions
+            fileExt:    'png|jpg|jpeg|gif',
+
+            animationSlide:   true,
+
+// animation speed in ms
+            animationSpeed: 250,
+
+// image preloading
+            preloading:   true,
+
+// keyboard navigation
+            enableKeyboard: true,
+
+// endless looping
+            loop:     true,
+
+// closes the lightbox when clicking outside
+            docClose:     true,
+
+// how much pixel you have to swipe
+            swipeTolerance: 50,
+
+// lightbox wrapper Class
+            className:    'simple-lightbox',
+
+// width / height ratios
+            widthRatio:   0.8,
+            heightRatio:  0.9,
+
+// disable right click
+            disableRightClick:  false,
+
+
+});
+
+    // Opens the lightbox
+    gallery.open();
+
+// Closes the lightbox
+    gallery.close();
+
+// Goto next image
+    gallery.next();
+
+// Goto previous image
+    gallery.prev();
+
+// re-init the lightbox
+    gallery.refresh();
+
+// Destroys the lightbox
+    gallery.destroy();
+
+
+});
