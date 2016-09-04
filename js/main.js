1325704741bdcb24e5c537ff7bdea97527cb36fe
KonshensX <mohamedbaza16@gmail.com>
2016-09-04 01:58:43 +0100

    Shitty background and navbar scroll pos changed to 100px

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
             var scroll_pos = 0;
             $(document).scroll(function() {
                 scroll_pos = $(this).scrollTop();
-                if(scroll_pos > 210) {
+                if(scroll_pos > 110) {
                     $("#navbar-primary").css('background-color', 'white');
                     $("#navbar-primary .navbar-nav > li").css('color', 'black');
                     $('.nav > li > a').css('padding', '0 30px');
